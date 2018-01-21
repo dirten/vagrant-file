1fb542396ede7ac684b06f317d45a83d4c0a4669
Jonathan <nataniel.greco@gmail.com>
2018-01-21 20:44:48 +0100

    Update vhost_custom.sh

--- V2/vhost_custom.sh
+++ V2/vhost_custom.sh
@@ -5,12 +5,21 @@ MY_WEB_CONFIG='server {
         listen [::]:80;
 
         root /var/www/website/public;
-        index index.html index.htm index.nginx-debian.html;
+        index index.php index.html index.htm index.nginx-debian.html;
 
         server_name website.local website.local;
 
         location / {
             try_files $uri $uri/ =404;
+        }
+
+        location ~ \.php$ {
+            include snippets/fastcgi-php.conf;
+            fastcgi_pass unix:/run/php/php7.1-fpm.sock;
+        }
+
+        location ~ /\.ht {
+            deny all;
         }
     }'
     echo "$MY_WEB_CONFIG" | sudo tee /etc/nginx/sites-available/website.local
