dbae274fc7bb16ef8b28a719124bd596ad6d3a36
Jonathan <nataniel.greco@gmail.com>
2018-02-01 18:17:03 +0100

    Delete postgres.sh

--- postgres.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# On met a jour les fichiers de configuration postgres que la connexion avec pgadmin se fasse facilement
-sudo echo "host all all all password" | sudo tee -a /etc/postgresql/9.3/main/pg_hba.conf > /dev/null
-sudo echo "listen_addresses = '*'"    | sudo tee -a /etc/postgresql/9.3/main/postgresql.conf > /dev/null
